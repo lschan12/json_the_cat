db476cfaff0dc11c13a984d8b746b315c36c0019
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-31 20:17:02 -0700

    Refactored the function with null values for errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,9 +4,10 @@ const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     const data = JSON.parse(body);
     if (data.length > 0) {
-      callback(error, data[0].description);
+      callback(null, data[0].description);
     } else {
-      callback(error, data[0]);
+      callback(error, null);
+      return;
     }
   });
 };
