ea0f223aec2aeb39048d5d191cd5b96d04c001bb
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-31 19:50:50 -0700

    implemented test functions to test that the request to catapi is working as intended

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,14 @@
 const request = require("request");
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  console.log(error);
-  const data = JSON.parse(body);
-  displayDetail(breed, data);
-});
-
-const displayDetail = (breed, data) => {
-  if (data.length < 1) {
-    console.log(`${breed} is not found.`);
-  } else {
-    console.log(data[0].description);
-  }
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length > 0) {
+      callback(error, data[0].description);
+    } else {
+      callback(error, data[0]);
+    }
+  });
 };
+
+module.exports = fetchBreedDescription;
